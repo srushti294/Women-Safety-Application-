09b3479abc08f3ac402d5a3514ba30ca6e8c5a68
srushti294 <80976142+srushti294@users.noreply.github.com>
2023-03-17 20:46:01 -0400

    Create about.java

---
+++ main/java/com/example/shesafety/about.java
@@ -0,0 +1,18 @@
+package com.example.shesafety.womensafety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class About extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        setTitle("About");
+
+
+
+    }
+}
